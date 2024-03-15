f32d5b214d2129801f68e1da1665190813505c41
mia-chou <mia.fy.chou@hotmail.com>
2024-03-15 13:06:34 +0000

    Updated code

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import QRCodeGenerator from "./components/qr-code-generator";
+
+function App() {
+  return (
+    <div className="App">
+
+
+       {/* QR code generator */}
+      {/* <QRCodeGenerator/> */}
+
+<ScrollToSection/>
+    </div>
+  );
+}
+
+export default App;
